f43f489d6b9bd2e8ca8ccacfee4977c90ab2b74e
Dana Ottaviani <dana.ottaviani@gmail.com>
2019-06-02 10:49:36 -0400

    Working on portfolio layout

--- src/pages/Portfolio.vue
+++ src/pages/Portfolio.vue
@@ -2,13 +2,15 @@
   <Layout>
     <b-container>
       <h1>Portfolio</h1>
-      <b-row>
-        <div class="grid" tabindex="0"></div>
-        <div class="grid" tabindex="0"></div>
-        <div class="grid" tabindex="0"></div>
-        <div class="grid" tabindex="0"></div>
-        <div class="grid" tabindex="0"></div>
-        <div class="grid" tabindex="0"></div>
+      <b-row class="odd-row">
+        <div class="grid" tabindex="0" v-for="i in 3" :key="i.id">
+          <div class="title">Project Title</div>
+        </div>
+      </b-row>
+      <b-row class="even-row">
+        <div class="grid" tabindex="0" v-for="i in 2" :key="i.id">
+          <div class="title">Project Title</div>
+        </div>
       </b-row>
     </b-container>
   </Layout>
@@ -33,15 +35,38 @@ h1 {
   justify-content: center;
 }
 .grid {
-  border: 1px dashed black;
   height: 200px;
   width: 200px;
   transform: rotateZ(45deg);
-  background-color: #00ffffd1;
+  background-color: #00ffff;
+  position: relative;
+  margin: 0 44px;
 
   &:hover,
   &:focus {
     z-index: 1;
+  }
+
+  .title {
+    transform: rotateZ(-45deg);
+    position: absolute;
+    top: 4rem;
+    left: 18%;
+    color: #0d2538 !important;
+  }
+}
+
+.even-row {
+  margin-top: -2rem;
+}
+
+@media screen and (max-width: 768px) {
+  .even-row {
+    margin-top: 0;
+  }
+
+  .grid {
+    margin: 0 20px;
   }
 }
 </style>
